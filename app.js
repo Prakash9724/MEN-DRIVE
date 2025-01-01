45a6e8c89c03cc42ba28344f018a77f4280856d7
prakash9724 <hellobrother91069@gmail.com>
2025-01-01 19:48:03 +0530

    Enhance user authentication with login functionality and cookie management
    - Added cookie-parser middleware for handling cookies.
    - Implemented login route in user.routes.js, including validation and password comparison.
    - Generated JWT token upon successful login and set it as a cookie.
    - Updated package.json and package-lock.json to include cookie-parser dependency.

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const app = express();
 const userRouter = require('./routes/user.routes')
 const dotenv = require('dotenv')
 dotenv.config();
-const connectionDb =  require('./config/db')
+const connectionDb =  require('./config/db');
+const cookieParser = require('cookie-parser');
 connectionDb();
 
 
 
 
-
+app.use(express.static('public'))
+app.use(cookieParser());
 app.set('view engine',"ejs")
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,7 @@ const { body, validationResult } = require("express-validator");
 const userModel = require("../models/user.model");
 
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 router.get("/register", (req, res) => {
   res.render("register");
@@ -17,7 +18,7 @@ router.post(
 
   async (req, res) => {
     const err = validationResult(req);
-    
+
     if (!err.isEmpty()) {
       return res
         .status(400)
@@ -29,10 +30,56 @@ router.post(
     const newUser = await userModel.create({
       username,
       email,
-      password:hashPassword,
+      password: hashPassword,
     });
     res.json(newUser);
   }
+);
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.post(
+  "/login",
+  body("username").trim().isLength({ min: 3 }),
+  body("password").trim().isLength({ min: 3 }),
+  async (req, res) => {
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+      return res.status(400).json({
+        err: err.array(),
+        message: "invalid Data",
+      });
+    }
+
+    const { username, password } = req.body;
+    const user = await userModel.findOne({
+      username: username
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Username or password is wrong",
+      });
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch){
+        return res.status(400).json({
+            message:"username or password is incorrect"
+        })
+    }
+
+
+    const token = jwt.sign({
+        userId : user._id,
+        email : user.email,
+        username : user.username
+    },process.env.JWT_SECRET)
+
+    res.cookie('token',token);
+    res.send('Logged in successfully');
+}
 );
 
 module.exports = router;
