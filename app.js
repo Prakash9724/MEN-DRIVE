e662ec2979addb2b0b811c19078304c93f495977
prakash9724 <hellobrother91069@gmail.com>
2025-01-01 17:51:59 +0530

    database connection

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express();
 const userRouter = require('./routes/user.routes')
+const dotenv = require('dotenv')
+dotenv.config();
+const connectionDb =  require('./config/db')
+connectionDb();
+
+
+
+
 
 app.set('view engine',"ejs")
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+
+
+
+
 app.use('/user',userRouter);
-app.listen(3000)
+app.listen(3000,()=>{
+    console.log("Server is running on 3000 port✨");
+    
+})
--- config/db.js
+++ config/db.js
@@ -1,3 +1,11 @@
 const mongoose =  require('mongoose');
 
 
+function connectionDb(){
+        mongoose.connect(process.env.MONGO_URI).then(()=>{
+            console.log("Conected to Database");
+            
+        })
+}
+
+module.exports = connectionDb
